16f2e8bd7644c94057243b1ceb611379227eaefb
Jordan O'Connor <jordanmoconnor@gmail.com>
2021-02-03 08:49:42 -0500

    working data retrieval

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
     let db = new sqlite3.Database('BTCUSD.sqlite3');
-    let sql = `SELECT DISTINCT Date date, Close close FROM tickers ORDER BY date`;
+    let sql = `SELECT DISTINCT Date t, Open o, High h, Low l, Close c FROM tickers WHERE date < 1483478160000 ORDER BY date`;
     let data = null;
 
     db.all(sql, [], (err, rows) => {
@@ -22,17 +22,10 @@ app.get("/", (req, res) => {
             console.log(err)
             throw err;
         } else {
-            data = rows;
-        }
-    }, (err, n) => {
-        if (err) {
-            throw err;
-        } else {
-            resolve(data);
             res.render("index", { data: data });
         }
-    });
-});
+    })
+})
 
 app.listen(3000, () => {
     console.log("server started on port 3000");
--- public/js/script.js
+++ public/js/script.js
@@ -1,37 +1,99 @@
-var ctx = document.getElementById('myChart');
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
+var barCount = 60;
+var initialDateStr = '01 Apr 2017 00:00 Z';
+
+var ctx = document.getElementById('chart').getContext('2d');
+ctx.canvas.width = 1000;
+ctx.canvas.height = 250;
+var chart = new Chart(ctx, {
+	type: 'candlestick',
+	data: {
+		datasets: [{
+			label: 'CHRT - Chart.js Corporation',
+			data: tickers
+		}]
+	}
+});
+
+var getRandomInt = function(max) {
+	return Math.floor(Math.random() * Math.floor(max));
+};
+
+function randomNumber(min, max) {
+	return Math.random() * (max - min) + min;
+}
+
+function randomBar(date, lastClose) {
+	var open = randomNumber(lastClose * 0.95, lastClose * 1.05).toFixed(2);
+	var close = randomNumber(open * 0.95, open * 1.05).toFixed(2);
+	var high = randomNumber(Math.max(open, close), Math.max(open, close) * 1.1).toFixed(2);
+	var low = randomNumber(Math.min(open, close) * 0.9, Math.min(open, close)).toFixed(2);
+	return {
+		t: date.valueOf(),
+		o: open,
+		h: high,
+		l: low,
+		c: close
+	};
+
+}
+
+function getRandomData(dateStr, count) {
+	var date = luxon.DateTime.fromRFC2822(dateStr);
+	var data = [randomBar(date, 30)];
+	while (data.length < count) {
+		date = date.plus({days: 1});
+		if (date.weekday <= 5) {
+			data.push(randomBar(date, data[data.length - 1].c));
+		}
+	}
+    console.log(data);
+	return data;
+}
+
+var update = function() {
+	var dataset = chart.config.data.datasets[0];
+
+	// candlestick vs ohlc
+	var type = document.getElementById('type').value;
+	dataset.type = type;
+
+	// linear vs log
+	var scaleType = document.getElementById('scale-type').value;
+	chart.config.options.scales.y.type = scaleType;
+
+	// color
+	var colorScheme = document.getElementById('color-scheme').value;
+	if (colorScheme === 'neon') {
+		dataset.color = {
+			up: '#01ff01',
+			down: '#fe0000',
+			unchanged: '#999',
+		};
+	} else {
+		delete dataset.color;
+	}
+
+	// border
+	var border = document.getElementById('border').value;
+	var defaultOpts = Chart.defaults.elements[type];
+	if (border === 'true') {
+		dataset.borderColor = defaultOpts.borderColor;
+	} else {
+		dataset.borderColor = {
+			up: defaultOpts.color.up,
+			down: defaultOpts.color.down,
+			unchanged: defaultOpts.color.up
+		};
+	}
+
+	chart.update();
+};
+
+document.getElementById('update').addEventListener('click', update);
+
+document.getElementById('randomizeData').addEventListener('click', function() {
+	chart.data.datasets.forEach(function(dataset) {
+		dataset.data = getRandomData(initialDateStr, barCount);
+	});
+	update();
 });
